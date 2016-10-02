98f06ce65f87fb788ae067a19390b111ea90d596
dpbabkin <dpbabkin@gmail.com>
2016-10-02 10:06:27 +0200

    temporary working solution.

--- src/main/groovy/org/gradle/okjava/buildtool/BuildConfigPlugin.groovy
+++ src/main/groovy/org/gradle/okjava/buildtool/BuildConfigPlugin.groovy
@@ -6,18 +6,13 @@ import org.gradle.api.Project
 class BuildConfigPlugin implements Plugin<Project> {
     void apply(Project project) {
 
-        project.ext.load = {
-            gradleConfigName ->
-                String urlToGradleConfigFile = project.buildscript.classLoader.getResource('gradle/' + gradleConfigName + '.gradle').toURI();
-                println 'BuildConfigPlugin.loading: ' + urlToGradleConfigFile;
-                project.apply(from: urlToGradleConfigFile);
-        }
-        project.ext.load('BuildConfigPlugin')
+        project.ext.load = { gradleConfigName -> load(gradleConfigName, project); };
+        load('BuildConfigPlugin', project);
     }
 
-//    void load(String configName) {
-//        String urlToGradleFile = project.buildscript.classLoader.getResource('gradle/' + configName + '.gradle').toURI();
-//        println 'BuildConfigPlugin.loading `' + configName + ' from file: ' + urlToGradleFile;
-//        project.apply(from: urlToGradleFile);
-//    }
+    private static void load(String gradleConfigName, Project project) {
+        String urlToGradleConfigFile = project.buildscript.classLoader.getResource('gradle/' + gradleConfigName + '.gradle').toURI();
+        println 'BuildConfigPlugin.loading `' + gradleConfigName + ' from file: ' + urlToGradleConfigFile;
+        project.apply(from: urlToGradleConfigFile);
+    }
 }
