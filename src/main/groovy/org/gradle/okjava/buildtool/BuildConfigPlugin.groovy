5f6c34a93bbc1bd46fc634e9a10dc55aca6bae05
dpbabkin <dpbabkin@gmail.com>
2016-10-02 11:54:33 +0200

    version works for other project

--- src/main/groovy/org/gradle/okjava/buildtool/BuildConfigPlugin.groovy
+++ src/main/groovy/org/gradle/okjava/buildtool/BuildConfigPlugin.groovy
@@ -4,16 +4,20 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class BuildConfigPlugin implements Plugin<Project> {
-    void apply(Project project) {
 
-        project.ext.load = { gradleConfigName -> load(gradleConfigName, project); };
-        load('BuildConfigPlugin', project);
+    String[] configs = { 'CommonConfig' };
+
+    void apply(Project project) {
+        project.ext.definedGretting = 'okjava build config plugin loaded';
+        println project.ext.definedGretting;
+        project.ext.load = { gradleConfigName -> load(gradleConfigName, project) };
+        configs.each { load(it, project) }
     }
 
     private static void load(String gradleConfigName, Project project) {
         URL url = project.buildscript.classLoader.getResource('gradle/' + gradleConfigName + '.gradle');
         if (url == null) {
-            println('Can not resolve gradle config with name: ' + gradleConfigName);
+            println('BuildConfigPlugin:Can not resolve gradle config with name: ' + gradleConfigName);
             return;
         }
         String urlToGradleConfigFile = url.toURI();
